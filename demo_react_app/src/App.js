cc7631e47995ce3591ea536f07c29186c6ebb251
Summyalena <summyalena@gmail.com>
2023-10-29 18:19:58 +0100

    work

--- demo_react_app/src/App.js
+++ demo_react_app/src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import FormPage from './FormPage';
-import Header from './Header';
+import "./App.css";
+import FormPage from "./comps/FormPage";
+import Header from "./comps/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <div className="splitcontainer">
-       <div  className='animated_texts'>
-        <p>Earn money while attending events</p>
-        <p>It's simple</p>
-        <p>Share links earn $$$</p>
+        <div className="animated_texts">
+          <p>Earn money while attending events</p>
+          <p>It's simple</p>
+          <p>Share links earn $$$</p>
         </div>
+
         <div>
           <span>About Split</span>
         </div>
-        <FormPage/>
+        <FormPage />
       </div>
     </div>
   );
 }
 
 export default App;
- 
